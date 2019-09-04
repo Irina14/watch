9ff0ed12110e1d82f4df38c2c8d3bc6d0193850b
Irina14 <ira1786@bk.ru>
2019-09-04 21:46:37 +0600

    Add section popular html

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,11 +1,34 @@
 const menu = () => {
+  const ESC_KEYCODE = 27;
   const menuSite = document.querySelector(`.menu-site`);
   const menuButton = document.querySelector(`.header__toggle`);
+
+  const documentClickHandler = (evt) => {
+    if (evt.target !== menuButton) {
+      menuSite.classList.remove(`menu-site--open`);
+      menuButton.classList.remove(`header__toggle--open`);
+    }
+  };
+
+  const documentEscKeyHandler = (evt) => {
+    if (evt.keyCode === ESC_KEYCODE) {
+      menuSite.classList.remove(`menu-site--open`);
+      menuButton.classList.remove(`header__toggle--open`);
+    }
+  };
 
   menuButton.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     menuSite.classList.toggle(`menu-site--open`);
     menuButton.classList.toggle(`header__toggle--open`);
+
+    if (menuSite.classList.contains(`menu-site--open`)) {
+      document.addEventListener(`click`, documentClickHandler);
+      document.addEventListener(`keydown`, documentEscKeyHandler);
+    } else {
+      document.removeEventListener(`click`, documentClickHandler);
+      document.removeEventListener(`keydown`, documentEscKeyHandler);
+    }
   });
 };
 
